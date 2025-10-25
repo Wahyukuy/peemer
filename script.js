8dade5313d26356ba3d20f5751ff6691cb99351d
Wahyukuy <105117758+Wahyukuy@users.noreply.github.com>
2025-10-25 11:58:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.getElementById("infoButton").addEventListener("click", function () {
-  alert("PMR aktif setiap hari Jumat pukul 15.00 di ruang UKS. Yuk gabung!");
+document.getElementById("tentang").addEventListener("click", function () {
+  window.location.href = "tentang.html";
+});
+
+document.getElementById("kegiatan").addEventListener("click", function () {
+  window.location.href = "kegiatan.html";
+});
+
+document.getElementById("kontak").addEventListener("click", function () {
+  window.location.href = "kontak.html";
+});
+
+document.getElementById("galeri").addEventListener("click", function () {
+  window.location.href = "galeri.html";
 });
